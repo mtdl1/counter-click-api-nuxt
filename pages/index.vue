233d471224fb5ddf8f6e2c998cca7c054f13fa84
Mateus Dubovicky <mateusdubovicky@hotmail.com>
2023-04-26 17:54:44 -0300

    Api com resposta

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>teste</h1>
+    <MyButton />
+    <p>{{ clicks }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'contato',
+  data() {
+    return {
+      clicks: null
+    }
+  },
+  async created() {
+    const response = await $fetch('/api/whatsapp-clicks')
+    console.log(response)
+    this.clicks = response.api
+  },
+}
+</script>
